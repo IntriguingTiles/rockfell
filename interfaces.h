7446341e0a578fc2a9bd2e299207403d50be2056
IntriguingTiles <jnickw@outlook.com>
2020-07-15 20:38:43 -0600

    Shut up GCC my code is awesome

--- interfaces.h
+++ interfaces.h
@@ -5,9 +5,11 @@
 class IRenderable {
 public:
 	virtual void Render(SDL_Renderer*) = 0;
+	virtual ~IRenderable() {};
 };
 
 class IEventListener {
 public:
 	virtual void Update(SDL_Event*) = 0;
+	virtual ~IEventListener() {};
 };
